6eef85e71f58a25514a9a615a2d2fbf8212b70a4
typeofNaN <dmdefine6@gmail.com>
2020-04-23 18:47:50 +0800

    add some page

--- client/src/router/modules/homeRouters.js
+++ client/src/router/modules/homeRouters.js
@@ -3,6 +3,12 @@ const DefaultLayout = () => import('@/layouts/default')
 const Home = () => import('@/views/home')
 const HomeForm = () => import('@/views/home/homeForm')
 const Message = () => import('@/views/home/message')
+const MessageBox = () => import('@/views/home/messageBox')
+const Notification = () => import('@/views/home/notification')
+const Tooltip = () => import('@/views/home/tooltip')
+const Popover = () => import('@/views/home/popover')
+const Popconfirm = () => import('@/views/home/popconfirm')
+const Calendar = () => import('@/views/home/calendar')
 
 const HomeRoute = {
   path: '/',
@@ -29,14 +35,86 @@ const HomeFormRoute = {
   }
 }
 
-const messageRoute = {
+const MessageRoute = {
   path: '/messagelayer',
-  name: 'messagelayer',
+  name: 'message_layer',
   component: Message,
   meta: {
     icon_white: 'control-white',
     icon_black: 'control-black',
     title: 'message_layer',
+    group: 'home'
+  }
+}
+
+const MessageBoxRoute = {
+  path: '/messagebox',
+  name: 'message_box',
+  component: MessageBox,
+  meta: {
+    icon_white: 'control-white',
+    icon_black: 'control-black',
+    title: 'message_box',
+    group: 'home'
+  }
+}
+
+const NotificationRoute = {
+  path: '/notification',
+  name: 'notification',
+  component: Notification,
+  meta: {
+    icon_white: 'control-white',
+    icon_black: 'control-black',
+    title: 'notification',
+    group: 'home'
+  }
+}
+
+const TooltipRoute = {
+  path: '/tooltip',
+  name: 'tooltip',
+  component: Tooltip,
+  meta: {
+    icon_white: 'control-white',
+    icon_black: 'control-black',
+    title: 'tooltip',
+    group: 'home'
+  }
+}
+
+const PopoverRoute = {
+  path: '/popover',
+  name: 'popover',
+  component: Popover,
+  meta: {
+    icon_white: 'control-white',
+    icon_black: 'control-black',
+    title: 'popover',
+    group: 'home'
+  }
+}
+
+const PopconfirmRoute = {
+  path: '/popconfirm',
+  name: 'popconfirm',
+  component: Popconfirm,
+  meta: {
+    icon_white: 'control-white',
+    icon_black: 'control-black',
+    title: 'popconfirm',
+    group: 'home'
+  }
+}
+
+const CalendarRoute = {
+  path: '/calendar',
+  name: 'calendar',
+  component: Calendar,
+  meta: {
+    icon_white: 'control-white',
+    icon_black: 'control-black',
+    title: 'calendar',
     group: 'home'
   }
 }
@@ -54,7 +132,13 @@ const HomeRouters = {
   children: [
     HomeRoute,
     HomeFormRoute,
-    messageRoute
+    MessageRoute,
+    MessageBoxRoute,
+    NotificationRoute,
+    TooltipRoute,
+    PopoverRoute,
+    PopconfirmRoute,
+    CalendarRoute
   ]
 }
 
